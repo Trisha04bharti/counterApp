be4f0bd2d27c3745f0488f187915a068b905d9b9
Trisha Bharti <trishabharti444@gmail.com>
2026-02-20 19:44:10 +0530

    Your message

--- counter/ContentView.swift
+++ counter/ContentView.swift
@@ -9,10 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var count : Int = 0
     
     var body: some View {
         
         VStack{
+            ZStack{
+                Circle()
+                    .fill(.brown)
+                    .frame(width: 150)
+                    .shadow(radius: 10)
+                Text("\(count)")
+                    .font(.largeTitle)
+                    
+            }
+            
+            HStack{
+                Button("increase value"){
+                    count += 1
+                }
+                .buttonStyle(.borderedProminent)
+                Button("decrease value"){
+                    count += 1
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+            }
             
         }
         
