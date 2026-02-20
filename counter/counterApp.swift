5d1bbbc100af8faca2a7f22b62e037a1dde06f68
iamvikramjeet1910 <chhotu123kota@gmail.com>
2026-02-20 17:02:08 +0530

    Initial Commit

---
+++ counter/counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterApp.swift
+//  counter
+//
+//  Created by Vikram Kumar on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
